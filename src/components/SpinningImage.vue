35e4eb6c7bfb37db47b4970df7c78d4f3048e303
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-06-16 18:25:19 -0400

    funny tests

--- src/components/SpinningImage.vue
+++ src/components/SpinningImage.vue
@@ -15,16 +15,54 @@ const setRandomImageSrc = async () => {
   console.log(image)
   currentTitle.value = image.match(/([a-zA-Z]+_[a-zA-Z]+)/)[0]
 }
-
-
 setRandomImageSrc()
+
+const angle = ref({ x: 1, y: 1 })
+
+const clickCoordinates = { x: 0, y: 0 }
+
+const mouseClick = (e) => {
+
+}
+
+const mouseDown = (e) => {
+  // const middleX = window.innerWidth / 2
+  // const distanceX = (e.clientX - middleX) / middleX
+
+  // const middleY = window.innerHeight / 2
+  // const distanceY = -(e.clientY - middleY) / middleY
+
+
+  // console.log(distanceX, distanceY)
+  // angle.value.x = distanceY
+  // angle.value.y = distanceX
+
+  clickCoordinates.x = e.clientX
+  clickCoordinates.y = e.clientY
+}
+
+const mouseUp = (e) => {
+  const x = (e.clientX - clickCoordinates.x) / window.innerWidth
+  const y = (e.clientY - clickCoordinates.y) / window.innerHeight
+
+  console.log(x, y)
+
+  angle.value.x = -y
+  angle.value.y = x
+}
+
+
 </script>
 
 <template>
   <h1>{{ currentTitle }}</h1>
-  <main class="main">
-    <img :src="currentImgSrc" class="spinningImage" @click="setRandomImageSrc()" />
+  <main class="main" @mousedown="mouseDown" @mouseup="mouseUp">
+    <div class="imageContainer">
+      <img :src="currentImgSrc" class="spinningImage" @click="setRandomImageSrc()" />
+    </div>
   </main>
+
+
 
 </template>
 
@@ -41,28 +79,61 @@ h1 {
   text-align: center;
 }
 
+.master {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: -1;
+}
+
+.imageContainer {
+  height: 50%;
+  /* animation: spin 10s linear infinite; */
+}
 
 .spinningImage {
   object-fit: contain;
   object-position: center;
-  width: 100%;
   height: 100%;
+  transition: all 1s ease;
   animation: spin 5s linear infinite;
+
+  user-select: none;
+
+  transform-style: preserve-3d;
+  filter: brightness(1);
+  box-shadow: -40px 80px 80px -10px rgba(0, 0, 0, 0.7);
+  /* transform: scale(1, 1); */
 }
 
 
 
 @keyframes spin {
   0% {
-    transform: rotate3d(1, 1, 1, 0deg);
+    transform: rotate3d(v-bind('angle.x'), v-bind('angle.y'), 0, 0deg);
+
   }
 
   50% {
-    transform: rotate3d(1, 1, 1, 180deg);
+    transform: rotate3d(v-bind('angle.x'), v-bind('angle.y'), 0, 180deg);
+
   }
 
   100% {
-    transform: rotate3d(1, 1, 1, 360deg);
+    transform: rotate3d(v-bind('angle.x'), v-bind('angle.y'), 0, 360deg);
+  }
+}
+
+@keyframes mirror {
+  25% {
+    filter: brightness(0.3);
+  }
+
+  75% {
+    filter: brightness(1);
   }
 }
 </style>
